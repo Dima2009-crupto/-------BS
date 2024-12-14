a2acac409e164648cb469fe80766ab86b49dddec
Dima2009-crupto <dimaparoldima1234@gmail.com>
2024-12-14 11:17:04 +0100

    1 1

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 from flask import Flask, render_template
-from sqlalchemy import SQLColumnExpression
+
+from data import data
+from data.base import create_db
+from data.data_to_db import write_data_to_db
+
 
 app = Flask(__name__)
+NAVIGETION = data.departures
 
 # @app.route("/", method=["GET","POST"])
 @app.get("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", departures=NAVIGETION, tours=data.tours )
 
 
-@app.get("/")
-def departure():
-    return render_template("departure.html")
+@app.get("/tour/<id>")
+def get_tour(id):
+    tour = data.tours.get(id)
+    return render_template("tour.html",departures=NAVIGETION, tour=tour)
 
 
-@app.get("/")
-def tour():
-    return render_template("tour.html")
+@app.get("/departure/<dep_eng>/")
+def departure(dep_eng):
+    tours = {}
+    for id, tour in data.tours.items():
+        if tours["departure"] == dep_eng:
+            tours[id] = tour
+            
+    return render_template("departure.html", departures=NAVIGETION, tours=tours)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    create_db()
+    write_data_to_db()
+    #app.run(debug=True)
